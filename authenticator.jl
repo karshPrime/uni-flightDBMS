64cd7c3b9114c0326bb89592a5eb394f8bf2b88e
ut-kr <utkarsh@cyberfear.com>
2023-10-18 22:00:23 +1100

    ✨ generate token

--- authenticator.jl
+++ authenticator.jl
@@ -8,12 +8,16 @@ using DBInterface
 
     #? only allowing these functions to be accessiable from other files
     export authenticate, connect, log
+
     #? connect with the admin database as system
     #! private function; can be accessed only from within this file
     function _connect_as_sys() 
     end
 
+    #? generate random 10 digit alphanumeric token
     function _token_gen()
+        charset = ['0':'9' ; 'a':'z' ; 'A':'Z']
+        return join([charset[rand(1:end)] for _ in 1:10])
     end
 
     #? check if entered password is infact right
