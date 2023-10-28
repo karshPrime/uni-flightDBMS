0602cc650feadeecb30d0ac745a9865fbfc832fb
ut-kr <utkarsh@cyberfear.com>
2023-10-29 03:04:46 +1100

    ✨interface structure
    code structure for the file

--- interface_public.jl
+++ interface_public.jl
@@ -1,3 +1,29 @@
 #! program interface for the general public.
+#* public should only have view access for certain attributes.
 
 using MySQL
+using DBInterface
+using Printf
+
+include("authenticator.jl")
+using .Auth
+
+include("interface_library.jl")
+using .Lib
+
+#? print help menu showing all possible commands
+function help_menu()
+end
+
+#? Understand user command
+function understand_input(input)
+end
+
+#? Understand user command
+function run_cmd(command, connection, fTable)
+end
+
+#? the main function
+function main()
+    connection = Auth.public()
+end
