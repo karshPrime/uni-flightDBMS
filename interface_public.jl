cc7c7dc1caee1a998b09e69eb4e80f71624098aa
ut-kr <utkarsh@cyberfear.com>
2023-10-29 03:06:51 +1100

    ✨ main()

--- interface_public.jl
+++ interface_public.jl
@@ -26,4 +26,31 @@ end
 #? the main function
 function main()
     connection = Auth.public()
+    
+    Lib.banner("Public")
+    printstyled("\nPress [?] for Help and [X] to Quit.\n"; color = :red)
+
+    while true
+        user_input = Lib.take_input('$')
+        
+        if user_input == "x"
+            printstyled("Goodbye~\n"; color = :light_blue)
+            break
+        end
+
+        if user_input == "?"
+            help_menu()
+            continue
+        end
+
+        sql_cmd = understand_input(user_input)
+
+        if sql_cmd[1] == true
+            run_cmd(sql_cmd[2], connection, sql_cmd[3])
+        else
+            Lib.print_error(sql_cmd[2])
+        end
+    end
 end
+
+main()
