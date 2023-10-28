dbaf7974bcd9f92b228dc1b6a5bd3e8abb332392
ut-kr <utkarsh@cyberfear.com>
2023-10-29 04:33:29 +1100

    🐛 using -> include
    syntax error

--- interface_admin.jl
+++ interface_admin.jl
@@ -1,7 +1,6 @@
 #! program interface for airline admins
 #* admins should only be able to view and edit data that they're permitted to.
 
-using authenticate.jl
 include("authenticator.jl")
 using .Auth
 
