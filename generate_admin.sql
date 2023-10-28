ff0e014d155df25c87f9a231dfb74d2458520ad0
ut-kr <utkarsh@cyberfear.com>
2023-10-29 03:09:00 +1100

    🔧sys -> app
    for better clarification with whats available  by default (like sys database)

--- generate_admin.sql
+++ generate_admin.sql
@@ -5,14 +5,14 @@ USE control_db;
 set autocommit=1;
 
 --!-----------------------------------------------------!-- Users & Roles --!---------!--
-DROP USER IF EXISTS 'sys'@'%';
-CREATE USER 'sys'@'%' IDENTIFIED BY 'secretfornoonetoknow';
+DROP USER IF EXISTS 'app'@'%';
+CREATE USER 'app'@'%' IDENTIFIED BY 'secretfornoonetoknow';
 
 DROP USER IF EXISTS 'executive'@'%';
 CREATE USER 'executive'@'%' IDENTIFIED BY 'secretfornoonetoknow';
 
-DROP ROLE IF EXISTS rSys, rExecutive;
-CREATE ROLE rSys, rExecutive;
+DROP ROLE IF EXISTS rApp, rExecutive;
+CREATE ROLE rApp, rExecutive;
 
 --!-----------------------------------------------------!-- Create Table --!----------!--
 
@@ -61,7 +61,7 @@ CREATE TABLE `Session` (
 
 -- for ethical reasons, no one should be able to edit system logs
 GRANT SELECT ON control_db.Logs TO rExecutive; -- executives can only view all logs
-GRANT INSERT ON control_db.Logs TO rSys; -- system can only append to the logs
+GRANT INSERT ON control_db.Logs TO rApp; -- system can only append to the logs
 
 -- executives should, however, be able to view and edit all other employee related info.
 GRANT ALL ON control_db.Profile TO rExecutive;
@@ -71,10 +71,11 @@ GRANT rExecutive TO executive;
 SET DEFAULT ROLE rExecutive TO executive;
 
 -- system has only read certain data- data that is required for the program to operate.
-GRANT SELECT ON control_db.Authentication TO rSys;
-GRANT SELECT ON control_db.Access TO rSys;
-GRANT SELECT (ID, fName, lName) ON control_db.Profile TO rSys;
-GRANT rSys TO sys;
-SET DEFAULT ROLE rSys TO sys;
+GRANT SELECT ON control_db.Authentication TO rApp;
+GRANT SELECT ON control_db.Access TO rApp;
+GRANT SELECT (ID, fName, lName) ON control_db.Profile TO rApp;
+GRANT ALL ON control_db.Session TO rApp;
+GRANT rApp TO app;
+SET DEFAULT ROLE rApp TO app;
 
 FLUSH PRIVILEGES;
