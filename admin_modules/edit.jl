9b955ceae7971484ff693154ae29b8f86baa7abb
ut-kr <utkarsh@cyberfear.com>
2023-11-03 19:25:20 +1100

    🐛fix edit cmd
    - updated Scrape module name to Common
    
    - printing row in colour
    
    - fixed actionLog identifier to result

--- admin_modules/edit.jl
+++ admin_modules/edit.jl
@@ -1,5 +1,9 @@
 #? all functions for Edit command : MySQL's UDPATE command
 module Edit
+    using MySQL
+    using DBInterface
+
+    include("COMMON.jl"); using .Common
     export run
 
     function _print_result(data, access)
@@ -7,22 +11,27 @@ module Edit
     end
 
     function run(userInput, accessLvl, connection)
-        table = Scrape.enter_a_table(userInput, "edit", "in", accessLvl)
+        table = Common.enter_a_table(userInput, "edit", "in")
         if table == 1 return 1; end
 
-        (tableInfo, primaryKey) = Scrape.primary_key(table, connection)
+        (tableInfo, primaryKey) = Common.primary_key(table, connection)
 
         # Prompt for the entry ID to modify
-        println("Enter $primaryKey for entry you wish to modify: ")
+        printstyled("Enter $primaryKey for entry you wish to modify: "; color = :yellow)
         id = chomp(readline())
 
+        printstyled("Enter == to leave the field unchanged\n"; color = :red)
+        
         # Prompt for changes
         for row in tableInfo
-            println("> $row[:Field]: ")
+            printstyled("> $(row[:Field]): "; color = :yellow)
             newData = chomp(readline())
+
+            if newData == "==" continue; end
+
             sqlCmd = "UPDATE $table SET $row[:Field]=   '$newData' WHERE $primaryKey = $id;"
             
-            actionLog = DBInterface.execute(connection, sqlCmd)
+            result = DBInterface.execute(connection, sqlCmd)
 
             _print_result(result, accessLvl)
         end
