c7ba26c77a455e3cc847c10ffc59d2bfc04de526
ut-kr <utkarsh@cyberfear.com>
2023-11-04 15:27:38 +1100

    ✨ print staff

--- admin_modules/staff.jl
+++ admin_modules/staff.jl
@@ -2,6 +2,7 @@
 module Staff
     using MySQL
     using DBInterface
+    using Printf
 
     include("../interface_library.jl"); using .Lib
     include("COMMON.jl"); using .Common
@@ -29,30 +30,47 @@ module Staff
         return (showPass, conditions)
     end
 
-    function _print_result(result)
-        #
+    function _print_result(result, showPass)
+        Lib.draw_border([5,20,11,15,15,8,11])
+        Lib.table_head(
+            ["ID","Password","Role","Name","Surname", "Gender","Phone"],
+            [2,17,8,12,12,5,8]
+        )
+        Lib.draw_border([5,20,11,15,15,8,11])
+        
+        for row in result
+            pass = showPass ? row[:password] : "***************"
+            
+                formattedRow = @sprintf("| %-3s | %-18s | %-9s | %-13s | %-13s | %-6s | %-9s |", 
+                row[:ID],
+                pass,
+                row[:accessLvl],
+                row[:fName],
+                row[:lName],
+                row[:gender],
+                row[:phone]
+            )
+
+            printstyled("$formattedRow\n", color = :light_cyan)
+        end
+        Lib.draw_border([5,20,11,15,15,8,11])
     end
 
     function run(userInput, accessLvl, connection)
         if accessLvl != 3 return; end
-        flip_exec_db(true, connection)
+        Common.flip_exec_db(true, connection)
 
         password = (("", ""), ("AU.password, ", "JOIN Authentication AS AU ON P.ID = AU.ID"))
 
         (passCondition, conditions) = _decode(userInput)
 
-        sqlCmd = """ 
-            SELECT  P.ID, A.accessLvl, $(password[passCondition][1]) P.fName, P.lName, P.gender, P.phone
-            FROM Profile AS P JOIN 
-            Access AS A ON P.ID = A.ID
-            $(password[passCondition][2])
-            $conditions ;
+        sqlCmd = """SELECT  P.ID, A.accessLvl, $(password[passCondition][1]) P.fName, P.lName, P.gender, P.phone
+            FROM Profile AS P JOIN Access AS A ON P.ID = A.ID
+            $(password[passCondition][2]) $conditions ;
         """ 
-        println(sqlCmd)
         result = DBInterface.execute(connection, sqlCmd)
 
-        flip_exec_db(false, connection) # switch back to flight_db
-
-        _print_result(result)
+        _print_result(result, passCondition==2)
+        Common.flip_exec_db(false, connection) # switch back to flight_db
     end
 end
