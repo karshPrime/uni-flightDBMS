45a1ac0162b9663bfdcff78bdc05581c2ce2789b
ut-kr <utkarsh@cyberfear.com>
2023-11-05 10:45:07 +1100

    🔧 updated with common
    updated the command with using Common module and not Scrape (as it did before)

--- admin_modules/remove.jl
+++ admin_modules/remove.jl
@@ -1,5 +1,9 @@
 #? all functions for Remove command : MySQL's DELETE command
 module Remove
+    using MySQL
+    using DBInterface
+    
+    include("COMMON.jl"); using .Common
     export run
 
     function _print_result(data, access)
@@ -7,16 +11,16 @@ module Remove
     end
 
     function run(userInput, accessLvl, connection)
-        table = Scrape.enter_a_table(userInput, "remove", "from", accessLvl)
+        table = Common.enter_a_table(userInput, "remove", "from")
         if table == 1 return 1; end
         
-        (~, primaryKey) = Scrape.primary_key(table, connection)
-        println("Enter $primaryKey for entry you wish to delete: ")
+        (~, primaryKey) = Common.primary_key(table, connection)
+        printstyled("Enter $primaryKey for entry you wish to delete: "; color = :yellow)
         id = chomp(readline())
         
         if Common.decline() return 2; end
 
-        sqlCmd = "DELETE FROM $tableName WHERE $primaryKey = $id;"
+        sqlCmd = "DELETE FROM $table WHERE $primaryKey = $id;"
 
         action = DBInterface.execute(connection, sqlCmd)
 
