072ab8499641f66fd5515e4b0a497d6adfe40c73
ut-kr <utkarsh@cyberfear.com>
2023-11-05 15:59:19 +1100

    ✨ logs cmd
    fixed sequence errors and added display functionality.
    
    also added self-log code

--- admin_modules/logs.jl
+++ admin_modules/logs.jl
@@ -1,18 +1,17 @@
 #? all functions to access Logs
 module Logs
-    using MySQL
-    using DBInterface
-
+    using Printf
     include("../interface_library.jl"); using .Lib
     include("COMMON.jl"); using .Common
+    
     export run
 
     #? understand user input and generate corresponding sql
     function _decode(userInput)
         options = Dict(
             "on"     => "date",
-            "at"   => "time",
-            "by"   => "authorID",
+            "at"     => "time",
+            "by"     => "authorID",
             "action" => "action",
             "record" => "record"
         )
@@ -29,7 +28,25 @@ module Logs
 
     #? display formatted result
     function _print_result(result)
-        #
+        Lib.draw_border([12,10,10,8,7,31,12])
+        Lib.table_head(
+            ["Date","Time","Author","Action","Table","Details","Record"],
+            [9,7,7,5,4,28,9]
+        )
+        Lib.draw_border([12,10,10,8,7,31,12])
+        for row in result
+            formattedRow = @sprintf("| %-10s | %-8s | %-8s | %-6s | %-5s | %-29s | %-10s |", 
+                row[:date],
+                row[:time],
+                row[:authorID],
+                row[:action],
+                row[:table],
+                row[:details],
+                row[:record]
+            )
+            printstyled("$formattedRow\n", color = :light_cyan)
+        end
+        Lib.draw_border([12,10,10,8,7,31,12])
     end
 
     #? command run
@@ -40,11 +57,12 @@ module Logs
         conditions = _decode(userInput)
 
         sqlCmd = "SELECT * FROM Logs $conditions ;"
-        println(sqlCmd)
-        result = DBInterface.execute(connection, sqlCmd)
+        result = Common.execute(connection, sqlCmd, accessLvl, true)
+        if result == 1 return 1; end
+        
+        _print_result(result)
 
         flip_exec_db(false, connection) # switch back to flight_db
-
-        _print_result(result)
+        return ["Logs", "View", conditions=="" ? "ALL" : replace(conditions[8:end], "'" => ""), "control_db"]
     end
 end
